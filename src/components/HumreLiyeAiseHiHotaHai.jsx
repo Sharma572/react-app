df39d04f379a66c7388ff39723f24c75cb800946
Raunak Sharma <raunakk572@gmail.com>
2022-08-24 16:24:32 +0530

    first commit

---
+++ src/components/HumreLiyeAiseHiHotaHai.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function HumreLiyeAiseHiHotaHai() {
+  return (
+    <div>
+        <h1>Humre Liye Aise Hi Hota Hai Components</h1>
+        <button>Click me</button>
+        <p>mein pargraph hu</p>
+    </div>
+  )
+}
---
+++ src/components/KuchBhi.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function KuchBhi() {
+  return (
+    <div>
+        <h1>Kuch Bhi component</h1>
+        <button>Mujhe click karo</button>
+        <p>Mein Paragraph hu</p>
+    </div>
+  )
+}
